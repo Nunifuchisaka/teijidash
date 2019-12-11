1ab214e321bffec9409450bcf4f63fb122ff7103
Nunifuchisaka <nunifuchisaka@gmail.com>
2019-12-11 21:22:27 +0900

    fix

--- htdocs/teijidash.js
+++ htdocs/teijidash.js
@@ -13,7 +13,7 @@ $(function(){
 
 
 /*
-## 
+## PX to VW
 */
 
 const PX2VW = Backbone.View.extend({
@@ -28,13 +28,20 @@ const PX2VW = Backbone.View.extend({
     this.$maxWidth.on("input paste", this.render);
   },
   render: function(){
+    const self = this;
     const px = this.$px.val(),
           maxWidth = this.$maxWidth.val(),
           vw = Math.round(px * 100 / maxWidth * 100) / 100;
     
-    this.$vw.val(vw + "vw").select();
-    document.execCommand("copy");//クリップボードにコピー
-    this.$px.focus();
+    console.log("px", px);
+    
+    this.$vw.val(vw + "vw");
+    
+    setTimeout(function(){
+      self.$vw.select();
+      document.execCommand("copy");//クリップボードにコピー
+      self.$px.focus();
+    }, 10);
   }
 });
 
